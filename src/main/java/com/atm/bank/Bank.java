bcba8c9841aa799c585bded7bb49fc818080f06c
Furkan Ongoren <on.fr2021@gmail.com>
2023-10-16 22:26:52 +0100

    Updated Bank class workingHours text

--- src/main/java/com/atm/bank/Bank.java
+++ src/main/java/com/atm/bank/Bank.java
@@ -1,18 +1,28 @@
 package com.atm.bank;
 
+import com.atm.utilities.ConsoleUtils;
+
 public class Bank {
+
+    /*
+     *****************************************
+     * Libraries / Classes
+     *****************************************
+     */
+    ConsoleUtils console = new ConsoleUtils();
 
     /*
      *****************************************
      * Private Fields / Bank Internal
      *****************************************
-    */
+     */
 
     private final String name = "XBankUniversal";
     private final String branchId = "XB13541";
     private final String address = "6595 Breitenberg Orchard, New Margo, MO 81496";
     private final String contactNumber = "850-615-1458";
-    private final String workingHours = "Monday - Sat: 9am - 3pm\n" + "Sunday: Closed";
+    private final String workingHours = "Mon - Sat: " + console.whiteBrightBackground + console.blackBold + " 9am - 3pm " + console.reset + "\n" +
+            "Sunday:"+ console.redBold + " Closed " + console.reset;
     private final String[] listOfEmployees = {
             "Millard Batz",
             "Kayce Quitzon",
