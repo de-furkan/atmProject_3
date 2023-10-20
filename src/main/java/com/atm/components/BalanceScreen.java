1a30b10114d4286208f83842509fb7e7b7500a09
Furkan Ongoren <on.fr2021@gmail.com>
2023-10-20 16:26:02 +0100

    Updated BalanceScreen class in components package
        - Linked DbUtils class
        - Created BalanceScreen() constructor -> contains reference to the
            database object for getting shared values
        - showBalanceMessage() uses LocalDate and LocalTime + gets the
            "current_amount" column from database with the appropriate entry
            to show the balance

--- src/main/java/com/atm/components/BalanceScreen.java
+++ src/main/java/com/atm/components/BalanceScreen.java
@@ -1,4 +1,92 @@
 package com.atm.components;
 
+import com.atm.bank.Bank;
+import com.atm.runner.Atm_Runner;
+import com.atm.utilities.ConsoleUtils;
+import com.atm.utilities.DbUtils;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class BalanceScreen {
+
+    /*
+     *****************************************
+     * Private Fields / Data
+     *****************************************
+     */
+    private DbUtils database;
+
+    /*
+     *****************************************
+     * Libraries / Classes
+     *****************************************
+     */
+    ConsoleUtils console = new ConsoleUtils();
+    Bank bank = new Bank();
+
+    /*
+     *****************************************
+     * Constructors
+     *****************************************
+     */
+
+    public BalanceScreen() {
+        this.database = Atm_Runner.database;
+    }
+
+    public void showBalanceMessage() {
+
+        //create date object
+        LocalDate dateNow = LocalDate.now();
+        //day
+        int day = dateNow.getDayOfMonth();
+        //month
+        int month = dateNow.getMonthValue();
+        //year
+        int year = dateNow.getYear();
+
+        //create time object
+        LocalTime timeNow = LocalTime.now();
+        //hour
+        int hour = timeNow.getHour();
+        //minute
+        int minute = timeNow.getMinute();
+        //second
+        int second = timeNow.getSecond();
+
+        //create date string
+        String date = day + "/" + month + "/" + year;
+
+        //create time string
+        String time = hour + ":" + minute + ":" + second;
+
+
+        System.out.println(
+                console.purpleBold +
+                        "Date" + "\t\t\t\t\ttime" +
+                        console.reset
+        );
+
+        String fullCardNumber = database.showFullCardNumber();
+        String maskedCardNumber = fullCardNumber.substring(0, fullCardNumber.length() - 4)
+                .replaceAll("\\d", "X") +
+                fullCardNumber.substring(fullCardNumber.length() - 4);
+
+        StringBuilder message = new StringBuilder();
+        message.append("\n")
+                .append(date).append("\t\t\t\t").append(time).append("\n")
+                .append(console.redBold).append(console.blackBackground).append("CARD NUMBER").append(console.reset).append("\n")
+                .append(console.blackBackground).append(console.greenBold).append(maskedCardNumber).append(console.reset).append("\n\n")
+                .append("TRANSACTION").append("\n")
+                .append("WITHDRAWAL CHECKING").append("\n")
+                .append(bank.getBankName().toUpperCase()).append("\n")
+                .append(console.greenBackground).append(console.blackBold).append("CASH\t\t\t\t")
+                .append("£").append(database.showEntryField("current_amount")).append(console.reset);
+
+        System.out.println(message);
+        SplashScreen splashScreen = new SplashScreen();
+        splashScreen.homeMenu();
+    }
 }
