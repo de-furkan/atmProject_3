8fed1b8cc50091127716005941f14295a598c4b6
Furkan Ongoren <on.fr2021@gmail.com>
2023-10-24 21:09:16 +0100

    Updated the TransferScreen class in components package
        - Added transferAmountCheck(): this will check that input amout to
        transfer is valid number, is not below 50 and not greater than 1500
    
        - createTransferTransaction(): calls a secondary method from the
        database called transferMoney() which accepts a single argument, the
        double data tyope coming from transferAmountCheck()

--- src/main/java/com/atm/components/TransferScreen.java
+++ src/main/java/com/atm/components/TransferScreen.java
@@ -1,4 +1,86 @@
 package com.atm.components;
 
+import com.atm.runner.Atm_Runner;
+import com.atm.utilities.ConsoleUtils;
+import com.atm.utilities.DbUtils;
+
+import java.util.Scanner;
+
 public class TransferScreen {
+
+    /*
+     *****************************************
+     * Private Fields / Data
+     *****************************************
+     */
+    private DbUtils database;
+
+    /*
+     *****************************************
+     * Libraries / Classes
+     *****************************************
+     */
+    ConsoleUtils console = new ConsoleUtils();
+    Scanner scanner = new Scanner(System.in);
+    SplashScreen splashScreen = new SplashScreen();
+
+    /*
+     *****************************************
+     * Constructors
+     *****************************************
+     */
+    public TransferScreen() {
+        this.database = Atm_Runner.database;
+    }
+
+    public String transferAmountCheck() {
+        //transfer message
+        System.out.println(
+                console.blackBackground + console.greenBold +
+                        " How much would you like to Transfer? "
+                        + console.reset + "\n\n"
+        );
+
+        // get the transfer amount
+        String transferAmount = scanner.nextLine();
+
+        // Check if the provided transfer amount is a number
+        while (true) {
+            if (!transferAmount.matches("\\d+")) {
+                System.out.println(transferAmount);
+                System.out.println(
+                        console.redBrightBackground + console.blackBold +
+                                " Invalid input. Please provide a numeric value: " +
+                                console.reset
+                );
+                transferAmount = scanner.nextLine();
+                // Check if the provided transfer amount is less than 5
+            } else if (Double.parseDouble(transferAmount) < 50) {
+                System.out.println(transferAmount);
+                System.out.println(
+                        console.redBrightBackground + console.blackBold +
+                                " Invalid input. The value must be greater than or equal to 50: " +
+                                console.reset
+                );
+                transferAmount = scanner.nextLine();
+                // Check if the provided transfer amount is greater than 1000
+            } else if (Double.parseDouble(transferAmount) > 1500) {
+                System.out.println(transferAmount);
+                System.out.println(
+                        console.redBrightBackground + console.blackBold +
+                                " Invalid input. The value must be less than or equal to 1500: " +
+                                console.reset
+                );
+                transferAmount = scanner.nextLine();
+            } else {
+                System.out.println(transferAmount);
+                return transferAmount;
+            }
+        }
+    }
+
+    public void createTransferTransaction() {
+        database.transferMoney(Double.parseDouble(transferAmountCheck()));
+        splashScreen.homeMenu();
+    }
 }
